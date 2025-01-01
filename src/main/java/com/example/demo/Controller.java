b0d70a241f12665c535564fbc1b93ca4d67ecad5
NguyenSang232 <sang0398605044@gmail.com>
2025-01-01 16:20:47 +0700

    Update file Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+
 import com.example.demo.Service.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -8,25 +9,32 @@ import java.util.List;
 public class Controller {
     @Autowired
     private BuildingService buildingService;
+
     @GetMapping(value = "/api/building/")
-    public List<Building> getBuilding(@RequestParam(name="name") String name){
+    public List<Building> getBuilding(@RequestParam(name = "name") String name) {
         List<Building> buildings = buildingService.buildings(name);
         return buildings;
     }
-//    private JdbcTemplate jdbcTemplate;
-//    public Controller(JdbcTemplate jdbcTemplate1) {
-//        this.jdbcTemplate = jdbcTemplate1;
-//    }
-//    @GetMapping("test")
-//    public String test(){
-//        String sql = "CREATE TABLE IF NOT EXISTS test_table (" +
-//                "name VARCHAR(25) PRIMARY KEY)";
-//        jdbcTemplate.execute(sql);
-//        return "Create Sucessful";
-//    }
+    // @GetMapping(value = "/api/building/")
+    // public List<Building> getBuilding(@RequestParam(name="name") String name){
+    // List<Building> buildings = buildingService.buildings(name);
+    // return buildings;
+    // }
+    // private JdbcTemplate jdbcTemplate;
+    // public Controller(JdbcTemplate jdbcTemplate1) {
+    // this.jdbcTemplate = jdbcTemplate1;
+    // }
+    // @GetMapping("test")
+    // public String test(){
+    // String sql = "CREATE TABLE IF NOT EXISTS test_table (" +
+    // "name VARCHAR(25) PRIMARY KEY)";
+    // jdbcTemplate.execute(sql);
+    // return "Create Sucessful";
+    // }
     // code focus on MVC help code can get it easily
-    //Presentation Layer ==> Controller (View, Model, Controller)
-    //Business Logic Layer ==> @Service (handle output data depends on Requirement of Client)
-    //Data Access Layer ==> @Repository ==> Repository Class
+    // Presentation Layer ==> Controller (View, Model, Controller)
+    // Business Logic Layer ==> @Service (handle output data depends on Requirement
+    // of Client)
+    // Data Access Layer ==> @Repository ==> Repository Class
 
 }
